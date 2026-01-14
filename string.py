fd531c9be8f063042f64821b37e055ec778eafa8
islamjohn42 <islomjohn.uz@gmail.com>
2026-01-14 18:12:30 +0500

    feat: Add string and methods

---
+++ string.py
@@ -0,0 +1,18 @@
+# f"{sting} {string}"
+# title, upper, lower, capitalize,
+# lstrip, rstrip, strip
+# input()
+
+first_name = "Ali"
+last_name = "Smith"
+print(f"{first_name} {last_name}")
+
+print(first_name.title())
+print(first_name.lower())
+print(first_name.upper())
+print(first_name.capitalize())
+
+text = "                 Lorem ipsum  "
+print(text.lstrip())
+print(text.rstrip())
+print(text.strip())
