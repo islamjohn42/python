7296bd651018df0852407eb97cb21c97f89d7a4b
islamjohn42 <islomjohn.uz@gmail.com>
2026-01-14 22:53:38 +0500

    feat: Add number and methods

---
+++ numbers.py
@@ -0,0 +1,22 @@
+# int() float() str() 
+# input()
+
+x, y, z, PI = 10, 0.5, 56, 3.14
+print(x, y, z, PI)
+
+# t_yil = int(input("Enter your birth year: "))
+# year = 2026 - t_yil
+# message = "Your age is " + str(year)
+# print(message)
+
+userNumber = int(input("Enter your number: "))
+square_num = userNumber ** 2
+cube_num = userNumber ** 3
+print(f"Square of number {userNumber} is: {square_num}")
+print(f"Cube of number {userNumber} is: {cube_num}")
+
+age = int(input("Enter your age: "))
+year = 2026 - age
+message = "Your birth is " + str(year)
+print(message)
+
