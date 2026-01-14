7b0fea49c5fe37eaad407f2825412392cd597959
islamjohn42 <islomjohn.uz@gmail.com>
2026-01-14 17:52:42 +0500

    feat: Add examples of variable

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Variables
+greeting = "Hello world"
+print(greeting)
+
+xabar = "Important info"
+print(xabar)
+xabar = "Updated info"
+print(xabar)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
